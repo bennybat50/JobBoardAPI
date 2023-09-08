f720db5ccd016854aa14a822fb432eaf595e8583
Young Batimehin <135424342+soloman1090@users.noreply.github.com>
2023-09-08 23:05:09 +0100

    df

--- base/settings.py
+++ base/settings.py
@@ -92,10 +92,16 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'resume',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
+
 
 REST_FRAMEWORK = {
     'NON_FIELD_ERRORS_KEY': 'error',
